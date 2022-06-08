5140297e1ec645857830eb4127db24521faa4606
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2022-06-08 22:35:38 +0300

    add popUp

--- src/components/service/ServiceItem.js
+++ src/components/service/ServiceItem.js
@@ -2,7 +2,11 @@ import '../../styles/service_styles/Serv.css'
 
 
 const ServiceItem = ({ post, icon }) => {
-
+    const [isOpen, setIsOpen] = useState(false);
+ 
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <div className="post">
             <div>
@@ -14,7 +18,22 @@ const ServiceItem = ({ post, icon }) => {
             <div className="post-content">
                 <span >{ post.description }</span>
             </div>
-            <a className="post-btn" href={post.buttonLink}>Read More</a>
+            
+            <input
+                className="popup-btn1"
+                type="button"
+                value="Read More"
+                onClick={togglePopup}
+                />
+                {isOpen && <Popup
+                content={<>
+                    <div className="popup-title"><b>{post.popup.title}</b></div>
+                    <div className="popup-description"><p>{post.popup.description}</p></div>
+                    
+                    <a className="post-btn" href={post.buttonLink}><b>Visit site</b></a>
+                </>}
+                handleClose={togglePopup}
+                />}
         </div>
     );
 };
